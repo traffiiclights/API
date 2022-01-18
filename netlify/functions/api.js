1daad4c05db157a50ffe719c79f911752e49c3ee
Maarsls <maffey.marcel@gmx.net>
2022-01-18 13:23:41 +0100

    npm next

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,7 +3,6 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE);
 
 const router = express.Router();
 router.get("/", (req, res) => {
